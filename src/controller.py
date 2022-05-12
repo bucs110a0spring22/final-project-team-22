c2c446a288b0152c15bda96a93cfc62611736877
Brian Seo <98533321+BrianYSeo@users.noreply.github.com>
2022-05-12 02:07:14 +0000

    made "play again" option work properly

--- src/controller.py
+++ src/controller.py
@@ -23,17 +23,15 @@ class Controller:
     self.background.blit(pygame.image.load('assets/background.png'),(0,0))
     
     #objects
-    self.player=player.Player(50,height/2,"assets/player2.png")
-
+    self.player=player.Player(50,self.height/2,"assets/player2.png")
     self.enemies = pygame.sprite.Group()
     self.bullets=pygame.sprite.Group()
-    self.bullet=bullet.Bullet(50,height/2,"assets/bullet.png")
+    self.bullet=bullet.Bullet(50,self.height/2,"assets/bullet.png")
 
     self.enemies_killed = 0
     self.number_of_enemies=5
     for i in range(self.number_of_enemies):
-      self.enemies.add(enemy.Enemy(random.randrange(width/2, width*0.9),random.randrange(height/2, height*0.9),"assets/enemy.png"))
-    
+      self.enemies.add(enemy.Enemy(random.randrange(self.width/2, self.width*0.9),random.randrange(self.height/2, self.height*0.9),"assets/enemy.png"))
     
     #self.bullets.add(bullet.Bullet(-10,-10,"assets/Bullet_drawing.png"))
     self.all_sprites=pygame.sprite.Group((self.player),tuple(self.enemies), (self.bullet))
@@ -62,14 +60,13 @@ class Controller:
               self.player.move_right()
           elif(event.key == pygame.K_SPACE):
             self.bullet.fired(self.player.rect.x,self.player.rect.y)
+            
       bullet_hit= pygame.sprite.spritecollide(self.bullet,self.enemies, True)
       
       if bullet_hit:
         for i in bullet_hit:
           i.kill()
           self.enemies_killed += 1
-
-      print(self.enemies_killed)
 
       if self.enemies_killed == self.number_of_enemies:
         self.gamestate = "GAMEOVER"
@@ -82,15 +79,26 @@ class Controller:
 
   def gameover(self):
     font = pygame.font.SysFont(None, 30)
-    text = font.render("Game Over, play again? press SPACE ", False, (0, 0, 0))
+    text = font.render("Game Over, play again? press R!", False, (0, 0, 0))
     self.screen.blit(text, (200, 300))
     pygame.display.flip()
-    while True:
+    
+    while self.gamestate == "GAMEOVER":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-              if(event.key == pygame.K_SPACE):
+              if(event.key == pygame.K_r):
+                self.player=player.Player(50,self.height/2,"assets/player2.png")
+                self.enemies = pygame.sprite.Group()
+                self.bullets=pygame.sprite.Group()
+                self.bullet=bullet.Bullet(50,self.height/2,"assets/bullet.png")
+            
+                self.enemies_killed = 0
+                self.number_of_enemies=5
+                for i in range(self.number_of_enemies):
+                  self.enemies.add(enemy.Enemy(random.randrange(self.width/2, self.width*0.9),random.randrange(self.height/2, self.height*0.9),"assets/enemy.png"))
+
+                self.all_sprites=pygame.sprite.Group((self.player),tuple(self.enemies), (self.bullet))
                 self.gamestate="RUNNING"
-                break
                 
--- src/enemy.py
+++ src/enemy.py
@@ -12,7 +12,7 @@ class Enemy(pygame.sprite.Sprite):
 
   def update(self):
 
-    move = random.randrange(0,100)
-    if move == 50:
+    move = random.randrange(0,40)
+    if move == 1:
       self.rect.x -= 1
       
