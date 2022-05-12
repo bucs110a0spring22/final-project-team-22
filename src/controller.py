3c2e477f97321c2eb73030cc52a6ba6d825c16ad
Brian Seo <98533321+BrianYSeo@users.noreply.github.com>
2022-05-12 03:13:00 +0000

    added losing gamestate, increased difficulty

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,4 @@
-import pygame,sys,random
+import pygame, sys, random
 from src import bullet, enemy, player
 
 class Controller:
@@ -19,10 +19,12 @@ class Controller:
     self.background.blit(pygame.image.load('assets/background.png'),(0,0))
     
     #objects
-    self.player=player.Player(50, self.height/2, "assets/player2.png")
+    self.player = player.Player(250, self.height/2, "assets/player2.png")
+    self.playergroup = pygame.sprite.Group()
+    self.playergroup.add(self.player)
     self.enemies = pygame.sprite.Group()
     self.bullets = pygame.sprite.Group()
-    self.bullet = bullet.Bullet(50, self.height/2, "assets/bullet.png")
+    self.bullet = bullet.Bullet(250, self.height/2, "assets/bullet.png")
 
     self.enemies_killed = 0
     self.number_of_enemies = 5
@@ -38,6 +40,8 @@ class Controller:
         self.gameloop()
       if self.gamestate == "WIN":
         self.win()
+      if self.gamestate == "LOSE":
+        self.lose()
 
   def gameloop(self):
     
@@ -58,11 +62,17 @@ class Controller:
           if(event.key == pygame.K_SPACE):
             self.bullet.fired(self.player.rect.x,self.player.rect.y)
             
-      bullet_hit = pygame.sprite.spritecollide(self.bullet,self.enemies, True)
+      bullet_hit = pygame.sprite.spritecollide(self.bullet, self.enemies, True)
       if bullet_hit:
         for i in bullet_hit:
           i.kill()
           self.enemies_killed += 1
+
+      enemy_hit = pygame.sprite.groupcollide(self.enemies, self.playergroup, False, True)
+      if enemy_hit:
+        for i in enemy_hit:
+          i.kill()
+          self.gamestate = "LOSE"
 
       if self.enemies_killed == self.number_of_enemies:
         self.gamestate = "WIN"
@@ -88,10 +98,12 @@ class Controller:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
               if(event.key == pygame.K_r):
-                self.player = player.Player(50,self.height/2,"assets/player2.png")
+                self.player = player.Player(250, self.height/2, "assets/player2.png")
+                self.playergroup = pygame.sprite.Group()
+                self.playergroup.add(self.player)
                 self.enemies = pygame.sprite.Group()
                 self.bullets = pygame.sprite.Group()
-                self.bullet = bullet.Bullet(50,self.height/2,"assets/bullet.png")
+                self.bullet = bullet.Bullet(250, self.height/2, "assets/bullet.png")
             
                 self.enemies_killed = 0
                 self.number_of_enemies = 5
@@ -102,4 +114,35 @@ class Controller:
                 self.gamestate = "RUNNING"
               elif(event.key == pygame.K_q):
                 sys.exit()
-                
+
+  def lose(self):
+    font = pygame.font.SysFont(None, 50)
+    text_1 = font.render("You lost! Press R to play again.", False, (255, 255, 255))
+    text_2 = font.render("Press Q to exit.", False, (255, 255, 255))
+
+    self.screen.blit(text_1, (220, 250))
+    self.screen.blit(text_2, (340,300))
+    pygame.display.flip()
+
+    while self.gamestate == "LOSE":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+              if(event.key == pygame.K_r):
+                self.player = player.Player(250, self.height/2, "assets/player2.png")
+                self.playergroup = pygame.sprite.Group()
+                self.playergroup.add(self.player)
+                self.enemies = pygame.sprite.Group()
+                self.bullets = pygame.sprite.Group()
+                self.bullet = bullet.Bullet(250, self.height/2, "assets/bullet.png")
+            
+                self.enemies_killed = 0
+                self.number_of_enemies = 5
+                for i in range(self.number_of_enemies):
+                  self.enemies.add(enemy.Enemy(random.randrange(self.width/2, self.width*0.9),random.randrange(self.height/5, self.height*0.9),"assets/enemy.png"))
+
+                self.all_sprites = pygame.sprite.Group((self.player),tuple(self.enemies), (self.bullet))
+                self.gamestate = "RUNNING"
+              elif(event.key == pygame.K_q):
+                sys.exit()
--- src/enemy.py
+++ src/enemy.py
@@ -13,7 +13,7 @@ class Enemy(pygame.sprite.Sprite):
 
   def update(self):
 
-    move = random.randrange(0,40)
+    move = random.randrange(0, 30)
     if move == 1:
-      self.rect.x -= 1
+      self.rect.x -= 3
       
